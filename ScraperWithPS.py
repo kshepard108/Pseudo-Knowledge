2ace3bb0b0b0b15f9a1a10db3f7ba6dd62f459e3
dovsiew <david.ovsiew@gmail.com>
2019-08-19 23:53:46 -0400

    Add files via upload

--- ScraperWithPS.py
+++ ScraperWithPS.py
@@ -6,10 +6,12 @@ import datetime as dt
 import time
 import pprint
 
+start_sub_search = time.time()
+
 api = PushshiftAPI()
 
 # Declare the keyword, subreddit, and time period you want to use for this test.
-keyword = 'Mueller'
+keyword = 'Hillary'
 sub = 'The_Donald'
 start_date = int(dt.datetime(2019, 2, 6).timestamp())
 end_date = int(dt.datetime(2019, 8, 6).timestamp())
@@ -30,6 +32,8 @@ for i in submissions:
     numResults = numResults + 1
 
 # Print the id of each submission and the total number of results.
+end_sub_search = time.time()
+print("Total submission search runtime: " + str(end_sub_search - start_sub_search))
 print(results)
 print(numResults)
 
@@ -37,20 +41,20 @@ print(numResults)
 '''------------------------------------------------------------------'''
 
 
-# Method to convert UTC time into comprehensible format for'created' column of table.
+# Method to convert UTC time into comprehensible format for 'created' column of table.
 def get_date(created):
     return dt.datetime.fromtimestamp(created)
 
 # Used to measure total time of process.
-start = time.time()
+start_comment_search = time.time()
 
 # Creates Reddit instance.
 # You can log into an account by completing the fields below.
-reddit = praw.Reddit(client_id='yMgv1KO-nhdG1Q',
-                     client_secret='Ckgf0nqesd5h3nNd_ZtB5RVDWE8',
-                     password='banc9RGB',
-                     user_agent='ScraperBot',
-                     username='Danger_Spongecake')
+reddit = praw.Reddit(client_id='client_id',
+                     client_secret='client_secret',
+                     password='password',
+                     user_agent='user_agent',
+                     username='username')
 
 # Instantiating the dictionary, which is converted into and saved as a CSV using pandas.
 storage_dict = {"title": [],
@@ -80,7 +84,7 @@ count = 0
 for r in results:
     if reddit.submission(id=r).comments.__len__() > 0:
         new_posts.append(reddit.submission(id=r))
-        print(r)
+        # print(r)
         count = count+1
     if count > 1000:
         break
@@ -93,9 +97,9 @@ thread_num = 1
 data_num = 0
 # Loop through the posts of interest and add info to dictionary.
 for post in new_posts:
-    print("Submission: " + str(post))
+    '''print("Submission: " + str(post))
     print("Thread num: " + str(thread_num))
-    print("Data num: " + str(data_num))
+    print("Data num: " + str(data_num))'''
     storage_dict["title"].append(post.title)
     storage_dict["author"].append(post.author)
     storage_dict["body"].append(post.selftext)
@@ -113,9 +117,9 @@ for post in new_posts:
     print("Ready to loop through comments!")
     # Loop through all comments on posts and add info to dictionary.
     post.comments.replace_more(limit=None)
-    print(len(post.comments.list()))
+    #print(len(post.comments.list()))
     for comment in post.comments.list():
-        print("Data num: " + str(data_num))
+        #print("Data num: " + str(data_num))
         storage_dict["title"].append('N/A')
         storage_dict["author"].append(comment.author)
         storage_dict["body"].append(comment.body)
@@ -129,19 +133,19 @@ for post in new_posts:
         storage_num = data_num - 1
         count = 1
         add = "t3_"
-        print("We're looking for the ID: " + str(comment.parent_id))
+        #print("We're looking for the ID: " + str(comment.parent_id))
         while (storage_dict["id"][storage_num]) != comment.parent_id[3:]:
-            print("The ID at data_num: " + str(storage_num) + " is: " + str(storage_dict["id"][storage_num]))
+            #print("The ID at data_num: " + str(storage_num) + " is: " + str(storage_dict["id"][storage_num]))
             if (storage_dict["parent_id"][storage_num]) == comment.parent_id:
-                print("We've got a match!")
+                #print("We've got a match!")
                 count = count + 1
             storage_num = storage_num - 1
         number_string = ''
-        print("number string is " + number_string)
+        #print("number string is " + number_string)
         number_string = number_string + str(storage_dict["number"][storage_num])
-        print("adding " + str(storage_dict["number"][storage_num]))
+        #print("adding " + str(storage_dict["number"][storage_num]))
         number_string = number_string + "_" + str(count)
-        print("adding " + str(count))
+        #print("adding " + str(count))
         storage_dict["number"].append(number_string)
         storage_dict["created"].append(comment.created_utc)
         data_num = data_num + 1
@@ -159,6 +163,7 @@ topics_data = storage_data.assign(timestamp=_timestamp)
 file_name = 'reddit_scrape_' + keyword + '.csv'
 
 topics_data.to_csv(file_name, index=False)
-end = time.time()
+end_comment_search = time.time()
 # Print total runtime.
-print("Total runtime: " + str(end - start))
+print("Total comment search runtime: " + str(end_comment_search - start_comment_search))
+print(numResults)
